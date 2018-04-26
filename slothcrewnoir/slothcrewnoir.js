bd4a74e7db1d456fd64b0c1389f52b83770e2576
Aimeri Baddouh <aimeri@fashionsloth.slothcrew.com>
2018-04-26 13:04:28 -0400

    updated usability and scaling of images

--- slothcrewnoir/slothcrewnoir.js
+++ slothcrewnoir/slothcrewnoir.js
@@ -1,4 +1,6 @@
 let urlBase = "https://res.cloudinary.com/slothcrew/image/upload/q_auto:low,w_auto/"
+let thumbSize = (document.querySelector('.pictureContent').offsetWidth >= 301) ? 120 : 300
+let thumbnailUrl = "https://res.cloudinary.com/slothcrew/image/upload/g_face,c_thumb,w_"+thumbSize +",h_" +thumbSize+"/"
 
 fetch('https://res.cloudinary.com/slothcrew/image/list/slothcrewnoir.json')
   .then(function(response) {
@@ -8,8 +10,11 @@ fetch('https://res.cloudinary.com/slothcrew/image/list/slothcrewnoir.json')
       console.log(myJson.resources.length)
       for(resource of myJson.resources) {
         let image = document.createElement('img')
+        let urlWithThumb = thumbnailUrl + resource.public_id + '.' + resource.format
         let url = urlBase + resource.public_id + '.' + resource.format
-        image.src = url
+        image.src = urlWithThumb
+        image.style.cursor = "pointer"
+        image.addEventListener("click", function (){window.open(url,"_self")})
         document.querySelector('#pictureboard').appendChild(image)
       }
       //let url = urlBase + myJson.resources[0].public_id + '.' + myJson.resources[0].format
