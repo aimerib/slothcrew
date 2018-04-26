c5c50e838ad5f80eb9df116bad2f19de32985555
Aimeri Baddouh <aimeri@fashionsloth.slothcrew.com>
2018-04-26 11:55:18 -0400

    Initial deploy of Slothcrew Noir

---
+++ slothcrewnoir/slothcrewnoir.js
@@ -0,0 +1,17 @@
+let urlBase = "https://res.cloudinary.com/slothcrew/image/upload/q_auto:low,w_auto/"
+
+fetch('http://res.cloudinary.com/slothcrew/image/list/slothcrewnoir.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+      console.log(myJson.resources.length)
+      for(resource of myJson.resources) {
+        let image = document.createElement('img')
+        let url = urlBase + resource.public_id + '.' + resource.format
+        image.src = url
+        document.querySelector('#pictureboard').appendChild(image)
+      }
+      //let url = urlBase + myJson.resources[0].public_id + '.' + myJson.resources[0].format
+    //document.querySelector('#pictureboard').innerHTML = "<img src=" + url + "></a>"
+  });
